2f4b0680ebff94de48d4950e6dc56af039bc9827
Pan <pancanugrahawicaksana@gmail.com>
2022-10-06 01:18:42 +0700

    little update

--- app/middleware/tokenProcess.js
+++ app/middleware/tokenProcess.js
@@ -1,13 +1,11 @@
-function parseJwt (token) {
+const parseJwt = token => {
     return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
 }
 const verifyScope = scope => {
     return (req, res, next) => {
         let access_token = req.headers["domain-access-token"];
-        if(access_token || scope){
-            let scopes = ((parseJwt(access_token).scopes).toString()).split(' ');        
-            scopes.includes(scope) ? next() : res.status(403).send({ message: "Permission denied"});
-        }
+        let scopes = ((parseJwt(access_token).scopes).toString()).split(' ');        
+        scopes.includes(scope) ? next() : res.status(403).send({ message: "Permission denied"});
     }
 };
 
